9b289e8466ce2a37785ff121eb5e1864902bea9d
Ninja <nmukneam@students.pitzer.edu>
2023-02-11 00:44:30 -0800

    first commit

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,11 +1,22 @@
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db, User
 
 
 cli = FlaskGroup(app)
 
 
+
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@cli.command("seed_db")
+def seed_db():
+    db.session.add(User(email="test@domain.org"))
+    db.session.commit()
+
 if __name__ == "__main__":
     cli()
-    app.run(debug=True, host='0.0.0.0', port=988813)
